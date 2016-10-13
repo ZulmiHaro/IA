2c37a6c83fe470e1aea0c950476a6141c24390c1
ZulmiHaro <zul_haro@outlook.es>
2016-10-13 17:55:00 -0500

    Código Base
    Datos por default

---
+++ RED DE HUPFIELD/RED DE HUPFIELD/Form1.Designer.vb
@@ -0,0 +1,410 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.TextBox16 = New System.Windows.Forms.TextBox()
+        Me.btnAnalizar = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.txtE2 = New System.Windows.Forms.TextBox()
+        Me.txtE1 = New System.Windows.Forms.TextBox()
+        Me.txtYo = New System.Windows.Forms.TextBox()
+        Me.txtYs = New System.Windows.Forms.TextBox()
+        Me.txtYs3 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPatronRsp = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.TextBox7)
+        Me.GroupBox1.Controls.Add(Me.TextBox8)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(26, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(228, 77)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PATRONES DE ENTRENAMIENTO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(15, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "B"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(15, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "A"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.Navy
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Location = New System.Drawing.Point(162, 47)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox5.TabIndex = 7
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Enabled = False
+        Me.TextBox6.Location = New System.Drawing.Point(162, 30)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox6.TabIndex = 6
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.Color.Navy
+        Me.TextBox7.Enabled = False
+        Me.TextBox7.Location = New System.Drawing.Point(122, 47)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox7.TabIndex = 5
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Enabled = False
+        Me.TextBox8.Location = New System.Drawing.Point(122, 30)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox8.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(47, 47)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.Navy
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(47, 30)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox4.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(7, 47)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Navy
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(7, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox13)
+        Me.GroupBox2.Controls.Add(Me.TextBox14)
+        Me.GroupBox2.Controls.Add(Me.TextBox15)
+        Me.GroupBox2.Controls.Add(Me.TextBox16)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(26, 121)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(228, 77)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "PATRÓN DE BÚSQUEDA"
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Enabled = False
+        Me.TextBox13.Location = New System.Drawing.Point(104, 47)
+        Me.TextBox13.Multiline = True
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox13.TabIndex = 3
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Enabled = False
+        Me.TextBox14.Location = New System.Drawing.Point(104, 30)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox14.TabIndex = 2
+        '
+        'TextBox15
+        '
+        Me.TextBox15.Enabled = False
+        Me.TextBox15.Location = New System.Drawing.Point(64, 47)
+        Me.TextBox15.Multiline = True
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox15.TabIndex = 1
+        '
+        'TextBox16
+        '
+        Me.TextBox16.BackColor = System.Drawing.Color.Navy
+        Me.TextBox16.Enabled = False
+        Me.TextBox16.Location = New System.Drawing.Point(64, 30)
+        Me.TextBox16.Name = "TextBox16"
+        Me.TextBox16.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox16.TabIndex = 0
+        '
+        'btnAnalizar
+        '
+        Me.btnAnalizar.FlatAppearance.BorderSize = 0
+        Me.btnAnalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAnalizar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAnalizar.Location = New System.Drawing.Point(565, 54)
+        Me.btnAnalizar.Name = "btnAnalizar"
+        Me.btnAnalizar.Size = New System.Drawing.Size(94, 58)
+        Me.btnAnalizar.TabIndex = 2
+        Me.btnAnalizar.Text = "ANALIZAR"
+        Me.btnAnalizar.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtE2)
+        Me.GroupBox3.Controls.Add(Me.txtE1)
+        Me.GroupBox3.Controls.Add(Me.txtYo)
+        Me.GroupBox3.Controls.Add(Me.txtYs)
+        Me.GroupBox3.Controls.Add(Me.txtYs3)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(281, 28)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(247, 170)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "RESULTADOS OBTENIDOS"
+        '
+        'txtE2
+        '
+        Me.txtE2.Enabled = False
+        Me.txtE2.Location = New System.Drawing.Point(63, 64)
+        Me.txtE2.Name = "txtE2"
+        Me.txtE2.Size = New System.Drawing.Size(140, 20)
+        Me.txtE2.TabIndex = 20
+        '
+        'txtE1
+        '
+        Me.txtE1.Enabled = False
+        Me.txtE1.Location = New System.Drawing.Point(63, 36)
+        Me.txtE1.Name = "txtE1"
+        Me.txtE1.Size = New System.Drawing.Size(140, 20)
+        Me.txtE1.TabIndex = 19
+        '
+        'txtYo
+        '
+        Me.txtYo.Enabled = False
+        Me.txtYo.Location = New System.Drawing.Point(63, 143)
+        Me.txtYo.Name = "txtYo"
+        Me.txtYo.Size = New System.Drawing.Size(140, 20)
+        Me.txtYo.TabIndex = 18
+        '
+        'txtYs
+        '
+        Me.txtYs.Enabled = False
+        Me.txtYs.Location = New System.Drawing.Point(63, 117)
+        Me.txtYs.Name = "txtYs"
+        Me.txtYs.Size = New System.Drawing.Size(140, 20)
+        Me.txtYs.TabIndex = 17
+        '
+        'txtYs3
+        '
+        Me.txtYs3.Enabled = False
+        Me.txtYs3.Location = New System.Drawing.Point(63, 88)
+        Me.txtYs3.Name = "txtYs3"
+        Me.txtYs3.Size = New System.Drawing.Size(140, 20)
+        Me.txtYs3.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(17, 140)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(26, 13)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Yo:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(18, 114)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(25, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Ys:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 88)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Ys3:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 62)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "E2:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "E1:"
+        '
+        'txtPatronRsp
+        '
+        Me.txtPatronRsp.Enabled = False
+        Me.txtPatronRsp.Location = New System.Drawing.Point(565, 161)
+        Me.txtPatronRsp.Multiline = True
+        Me.txtPatronRsp.Name = "txtPatronRsp"
+        Me.txtPatronRsp.Size = New System.Drawing.Size(83, 37)
+        Me.txtPatronRsp.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(567, 145)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(62, 13)
+        Me.Label8.TabIndex = 5
+        Me.Label8.Text = "PATRÓN "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(680, 210)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtPatronRsp)
+        Me.Controls.Add(Me.btnAnalizar)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "R.N. HUPFIELD"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents btnAnalizar As Button
+    Friend WithEvents txtPatronRsp As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtYs3 As TextBox
+    Friend WithEvents txtYo As TextBox
+    Friend WithEvents txtYs As TextBox
+    Friend WithEvents txtE2 As TextBox
+    Friend WithEvents txtE1 As TextBox
+End Class
---
+++ RED DE HUPFIELD/RED DE HUPFIELD/Form1.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form1
+    Dim E1(0, 3) As Integer
+    Dim E2(0, 3) As Integer
+    Dim E3(0, 3) As Integer
+    Dim ET1(3, 0) As Integer
+    Dim ET2(3, 0) As Integer
+    Dim Mi(3, 3) As Integer
+    Dim Temp1(3, 3) As Integer
+    Dim Temp2(3, 3) As Integer
+    Dim W1(3, 3) As Integer
+    Dim W2(3, 3) As Integer
+    Dim Wap(3, 3) As Integer
+    Dim Ys3(0, 3) As Integer
+    Dim Ys(0, 3) As Integer
+    Dim Yo(0, 3) As Integer
+    Dim i, j, fila, columna As Integer
+    Private Sub btnAnalizar_Click(sender As Object, e As EventArgs) Handles btnAnalizar.Click
+        txtPatronRsp.Text = "          A"
+        Call llenaDatos()
+        Call transpuesta()
+        Call multiplicaT()
+        Call resta()
+        Call Funcion_Wap()
+        Call Hallar_Ys3()
+        Call Hallar_Yo()
+        Call ComprobarAprendizaje()
+
+    End Sub
+    Private Sub llenaDatos()
+        'Para el vector E1
+        E1(0, 0) = 1
+        E1(0, 1) = 1
+        E1(0, 2) = -1
+        E1(0, 3) = -1
+        txtE1.Text = "[" & E1(0, 0) & "  " & E1(0, 1) & "  " & E1(0, 2) & "  " & E1(0, 3) & "]"
+        'Para el vector E2
+        E2(0, 0) = -1
+        E2(0, 1) = -1
+        E2(0, 2) = 1
+        E2(0, 3) = 1
+        txtE2.Text = "[" & E2(0, 0) & "  " & E2(0, 1) & "  " & E2(0, 2) & "  " & E2(0, 3) & "]"
+        'Para el vector E3
+        E3(0, 0) = 1
+        E3(0, 1) = -1
+        E3(0, 2) = -1
+        E3(0, 3) = -1
+        'MATRIZ IDENTIDAD
+        'Mi(0, 0) = 1
+        'Mi(0, 1) = 0
+        'Mi(0, 2) = 0
+        'Mi(0, 3) = 0
+        'Mi(1, 0) = 0
+        'Mi(1, 1) = 1
+        'Mi(1, 2) = 0
+        'Mi(1, 3) = 0
+        'Mi(2, 0) = 0
+        'Mi(2, 1) = 0
+        'Mi(2, 2) = 1
+        'Mi(2, 3) = 0
+        'Mi(3, 0) = 0
+        'Mi(3, 1) = 0
+        'Mi(3, 2) = 0
+        'Mi(3, 3) = 1
+        For i = 0 To 3
+            For j = 0 To 3
+                If i = j Then
+                    Mi(i, j) = 1
+                Else
+                    Mi(i, j) = 0
+                End If
+            Next
+        Next
+    End Sub
+    Private Sub transpuesta()
+        'Para el vector TE1
+        ET1(0, 0) = E1(0, 0)
+        ET1(1, 0) = E1(0, 1)
+        ET1(2, 0) = E1(0, 2)
+        ET1(3, 0) = E1(0, 3)
+        'Para el vector TE2
+        ET2(0, 0) = E2(0, 0)
+        ET2(1, 0) = E2(0, 1)
+        ET2(2, 0) = E2(0, 2)
+        ET2(3, 0) = E2(0, 3)
+    End Sub
+    Private Sub multiplicaT()
+        'MATRIZ Temporal 1
+        Temp1(0, 0) = ET1(0, 0) * E1(0, 0)
+        Temp1(0, 1) = ET1(0, 0) * E1(0, 1)
+        Temp1(0, 2) = ET1(0, 0) * E1(0, 2)
+        Temp1(0, 3) = ET1(0, 0) * E1(0, 3)
+        Temp1(1, 0) = ET1(1, 0) * E1(0, 0)
+        Temp1(1, 1) = ET1(1, 0) * E1(0, 1)
+        Temp1(1, 2) = ET1(1, 0) * E1(0, 2)
+        Temp1(1, 3) = ET1(1, 0) * E1(0, 3)
+        Temp1(2, 0) = ET1(2, 0) * E1(0, 0)
+        Temp1(2, 1) = ET1(2, 0) * E1(0, 1)
+        Temp1(2, 2) = ET1(2, 0) * E1(0, 2)
+        Temp1(2, 3) = ET1(2, 0) * E1(0, 3)
+        Temp1(3, 0) = ET1(3, 0) * E1(0, 0)
+        Temp1(3, 1) = ET1(3, 0) * E1(0, 1)
+        Temp1(3, 2) = ET1(3, 0) * E1(0, 2)
+        Temp1(3, 3) = ET1(3, 0) * E1(0, 3)
+        'For i = 0 To 3
+        '    For j = 0 To 3
+        '        Temp1(i, j) = ET1(i, j) * E1(i, j)
+        '    Next
+        'Next
+        'MATRIZ Temporal 2
+        Temp2(0, 0) = ET2(0, 0) * E2(0, 0)
+        Temp2(0, 1) = ET2(0, 0) * E2(0, 1)
+        Temp2(0, 2) = ET2(0, 0) * E2(0, 2)
+        Temp2(0, 3) = ET2(0, 0) * E2(0, 3)
+        Temp2(1, 0) = ET2(1, 0) * E2(0, 0)
+        Temp2(1, 1) = ET2(1, 0) * E2(0, 1)
+        Temp2(1, 2) = ET2(1, 0) * E2(0, 2)
+        Temp2(1, 3) = ET2(1, 0) * E2(0, 3)
+        Temp2(2, 0) = ET2(2, 0) * E1(0, 0)
+        Temp2(2, 1) = ET2(2, 0) * E2(0, 1)
+        Temp2(2, 2) = ET2(2, 0) * E2(0, 2)
+        Temp2(2, 3) = ET2(2, 0) * E2(0, 3)
+        Temp2(3, 0) = ET2(3, 0) * E2(0, 0)
+        Temp2(3, 1) = ET2(3, 0) * E2(0, 1)
+        Temp2(3, 2) = ET2(3, 0) * E2(0, 2)
+        Temp2(3, 3) = ET2(3, 0) * E2(0, 3)
+    End Sub
+    Private Sub resta()
+        'MATRIZ w1
+        W1(0, 0) = Temp1(0, 0) - Mi(0, 0)
+        W1(0, 1) = Temp1(0, 1) - Mi(0, 1)
+        W1(0, 2) = Temp1(0, 2) - Mi(0, 2)
+        W1(0, 3) = Temp1(0, 3) - Mi(0, 3)
+        W1(1, 0) = Temp1(1, 0) - Mi(1, 0)
+        W1(1, 1) = Temp1(1, 1) - Mi(1, 1)
+        W1(1, 2) = Temp1(1, 2) - Mi(1, 2)
+        W1(1, 3) = Temp1(1, 3) - Mi(1, 3)
+        W1(2, 0) = Temp1(2, 0) - Mi(2, 0)
+        W1(2, 1) = Temp1(2, 1) - Mi(2, 1)
+        W1(2, 2) = Temp1(2, 2) - Mi(2, 2)
+        W1(2, 3) = Temp1(2, 3) - Mi(2, 3)
+        W1(3, 0) = Temp1(3, 0) - Mi(3, 0)
+        W1(3, 1) = Temp1(3, 1) - Mi(3, 1)
+        W1(3, 2) = Temp1(3, 2) - Mi(3, 2)
+        W1(3, 3) = Temp1(3, 3) - Mi(3, 3)
+        'MATRIZ w2
+        W2(0, 0) = Temp2(0, 0) - Mi(0, 0)
+        W2(0, 1) = Temp2(0, 1) - Mi(0, 1)
+        W2(0, 2) = Temp2(0, 2) - Mi(0, 2)
+        W2(0, 3) = Temp2(0, 3) - Mi(0, 3)
+        W2(1, 0) = Temp2(1, 0) - Mi(1, 0)
+        W2(1, 1) = Temp2(1, 1) - Mi(1, 1)
+        W2(1, 2) = Temp2(1, 2) - Mi(1, 2)
+        W2(1, 3) = Temp2(1, 3) - Mi(1, 3)
+        W2(2, 0) = Temp2(2, 0) - Mi(2, 0)
+        W2(2, 1) = Temp2(2, 1) - Mi(2, 1)
+        W2(2, 2) = Temp2(2, 2) - Mi(2, 2)
+        W2(2, 3) = Temp2(2, 3) - Mi(2, 3)
+        W2(3, 0) = Temp2(3, 0) - Mi(3, 0)
+        W2(3, 1) = Temp2(3, 1) - Mi(3, 1)
+        W2(3, 2) = Temp2(3, 2) - Mi(3, 2)
+        W2(3, 3) = Temp2(3, 3) - Mi(3, 3)
+    End Sub
+    Private Sub Funcion_Wap()
+        For i = 0 To 3
+            For j = 0 To 3
+                Wap(i, j) = W1(i, j) + W2(i, j)
+
+            Next
+        Next
+    End Sub
+    Private Sub Hallar_Ys3()
+        Ys3(0, 0) = E3(0, 0) * Wap(0, 0) + E3(0, 1) * Wap(1, 0) + E3(0, 2) * Wap(2, 0) + E3(0, 3) * Wap(3, 0)
+        Ys3(0, 1) = E3(0, 0) * Wap(0, 1) + E3(0, 1) * Wap(1, 1) + E3(0, 2) * Wap(2, 1) + E3(0, 3) * Wap(3, 1)
+        Ys3(0, 2) = E3(0, 0) * Wap(0, 2) + E3(0, 1) * Wap(1, 2) + E3(0, 2) * Wap(2, 2) + E3(0, 3) * Wap(3, 3)
+        Ys3(0, 3) = E3(0, 0) * Wap(0, 3) + E3(0, 1) * Wap(1, 3) + E3(0, 2) * Wap(2, 3) + E3(0, 3) * Wap(3, 3)
+        txtYs3.Text = "[" & Ys3(0, 0) & "  " & Ys3(0, 1) & "  " & Ys3(0, 2) & "  " & Ys3(0, 3) & "]"
+    End Sub
+    Private Sub Hallar_Yo()
+        If Ys3(0, 0) >= 0 Then
+            Yo(0, 0) = 1
+        Else
+            Yo(0, 0) = -1
+        End If
+        If Ys3(0, 1) >= 0 Then
+            Yo(0, 1) = 1
+        Else
+            Yo(0, 1) = -1
+        End If
+        If Ys3(0, 2) >= 0 Then
+            Yo(0, 2) = 1
+        Else
+            Yo(0, 2) = -1
+        End If
+        If Ys3(0, 3) >= 0 Then
+            Yo(0, 3) = 1
+        Else
+            Yo(0, 3) = -1
+        End If
+        txtYo.Text = "[" & Yo(0, 0) & "  " & Yo(0, 1) & "  " & Yo(0, 2) & "  " & Yo(0, 3) & "]"
+    End Sub
+    Private Sub ComprobarAprendizaje()
+        Ys(0, 0) = Yo(0, 0) * Wap(0, 0) + Yo(0, 1) * Wap(1, 0) + Yo(0, 2) * Wap(2, 0) + Yo(0, 3) * Wap(3, 0)
+        Ys(0, 1) = Yo(0, 0) * Wap(0, 1) + Yo(0, 1) * Wap(1, 1) + Yo(0, 2) * Wap(2, 1) + Yo(0, 3) * Wap(3, 1)
+        Ys(0, 2) = Yo(0, 0) * Wap(0, 2) + Yo(0, 1) * Wap(1, 2) + Yo(0, 2) * Wap(2, 2) + Yo(0, 3) * Wap(3, 3)
+        Ys(0, 3) = Yo(0, 0) * Wap(0, 3) + Yo(0, 1) * Wap(1, 3) + Yo(0, 2) * Wap(2, 3) + Yo(0, 3) * Wap(3, 3)
+        txtYs.Text = "[" & Ys(0, 0) & "  " & Ys(0, 1) & "  " & Ys(0, 2) & "  " & Ys(0, 3) & "]"
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+End Class
